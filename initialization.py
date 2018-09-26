865778f17a6410e0a79572d7c1537543425c55b2
ljp <ljp2@lehigh.edu>
2018-09-25 22:35:13 -0400

    initial commit

---
+++ initialization.py
@@ -0,0 +1,24 @@
+from zipcode import Zipcode
+from location import Location
+import params
+import setup
+
+
+#----------------------------------------------------------------------
+def init():
+    """"""
+    for location in Location.locations.values():
+        location.demand_external = 0.0
+        location.demand = location.demand_internal
+        location.prob_inventory_above_reserve = params.PDTUSA_LOW
+        location.prob_part_available = params.PDTUSA_LOW
+        
+        location.calculateIstar()
+        
+        
+
+    
+if __name__ == '__main__':
+    part_number = 'PN2'
+    setup.setup(part_number)
+    init()
